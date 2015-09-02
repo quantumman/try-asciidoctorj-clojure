b39b7c5c4319b39531115dabc04e97af3d50789f
Shodai Yokoyama <quantumcars@gmail.com>
2015-09-02 10:02:30 +0900

    handler.clj: Add an entrypoint for uberjar

--- src/try_asciidoctorj/handler.clj
+++ src/try_asciidoctorj/handler.clj
@@ -1,7 +1,9 @@
 (ns try-asciidoctorj.handler
+  (:gen-class)
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [org.httpkit.server :refer [run-server]]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
@@ -9,3 +11,7 @@
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+(defn -main [& [port]]
+  (let [port (Integer/parseInt (or port (System/getenv "PORT") "8080"))]
+    (run-server app {:port port})))
