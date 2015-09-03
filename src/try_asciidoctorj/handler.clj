436df982a549211a9767b9a01f77bc8459d46622
Shodai Yokoyama <quantumcars@gmail.com>
2015-09-03 09:58:02 +0900

    handler.clj: Render posted asciidoc

--- src/try_asciidoctorj/handler.clj
+++ src/try_asciidoctorj/handler.clj
@@ -5,13 +5,30 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.util.anti-forgery :refer :all]
             [org.httpkit.server :refer [run-server]]
-            [net.cgrand.enlive-html :as html]))
+            [net.cgrand.enlive-html :as html])
+  (:import org.asciidoctor.Asciidoctor$Factory
+           org.asciidoctor.Attributes
+           org.asciidoctor.Options
+           java.util.Collections))
+
+(def ^:private ^:static adoc (Asciidoctor$Factory/create))
 
 (html/deftemplate main-template "templates/application.html"
   []
   [:form]
   (html/prepend (html/html-snippet (anti-forgery-field))))
 
+(defn make-options
+  [doctype]
+  (if-not (clojure.string/blank? doctype)
+    (let [attributes (doto (Attributes.) (.setBackend doctype)),
+          options (doto (Options.) (.setAttributes attributes))]
+      options)
+    (Collections/emptyMap)
+    ))
+(defn render-page
+  [doc doctype]
+  (.convert adoc doc (make-options doctype)))
 
 (defn index-page
   []
@@ -19,6 +36,7 @@
 
 (defroutes app-routes
   (GET "/" [] (index-page))
+  (POST "/" [source doctype] (render-page source doctype))
   (route/not-found "Not Found"))
 
 (def app
